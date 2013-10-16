f37cd74d3793af114aac403f7ac24376aef32d74
Matthew Rayermann <matthew.rayermann@utexas.edu>
2013-10-15 19:50:26 -0500

    Update leaderboardManager.py

--- leaderboardManager.py
+++ leaderboardManager.py
@@ -1,4 +1,4 @@
-#!/user/bin/env python
+#!/usr/bin/env python
 
 from time import sleep
 from time import time
@@ -6,37 +6,44 @@ import string
 from parse_rest.connection import register
 from parse_rest.datatypes import Object
 
-while 1:
-  field = ('veryEasy', 'veryEasyPerfect', 
-           'easy', 'easyPerfect', 
-           'medium', 'mediumPerfect',
-           'hard', 'hardPerfect',
-           'insane', 'insanePerfect',
-           'numAchievements')
-  category = ('Very Easy:', 'Very Easy Perfect', 
-              'Easy', 'Easy Perfect', 
-              'Medium', 'Medium Perfect',
-              'Hard', 'Hard Perfect',
-              'Insane', 'Insane Perfect',
-              'Num Achievements')
-    
-  register(#keys redacted)
-  leaderboardString = ''
-  print time()
-  try:
-	#Query Db For All Player Stats
-  allStats = PlayerStats.Query.all()
-	print 'Query done.'
-  #Go through each field, and construct ranking
-  for i in range(1, len(field)):
-    print 'Building stats for ', category[i], '.\n';
-    leaderboardString += (category[i] + '\n')
-    currentRankings = allStats.order_by('-' + field[i])
-    rank = 1
-    for playerStat in currentRankings:
-      if playerStat.name:
-        leaderboardString += (str(rank) + '. ' + playerStat.name + ' ' + str(playerStat.__dict__[field[i]]) + '\n')
-        rank += 1
-      if rank > 5:
-        break
-  sleep(30)
+class PlayerStats(Object):
+  pass
+  
+class LeaderboardObject(Object):
+  pass
+
+if __name__ == '__main__':
+   while 1:
+     field = ('veryEasy', 'veryEasyPerfect', 
+              'easy', 'easyPerfect', 
+              'medium', 'mediumPerfect',
+              'hard', 'hardPerfect',
+              'insane', 'insanePerfect',
+              'numAchievements')
+     category = ('Very Easy:', 'Very Easy Perfect', 
+                 'Easy', 'Easy Perfect', 
+                 'Medium', 'Medium Perfect',
+                 'Hard', 'Hard Perfect',
+                 'Insane', 'Insane Perfect',
+                 'Num Achievements')
+       
+     register('keys redacted')
+     leaderboardString = ""
+     #print time()
+     #Query Db For All Player Stats
+     allStats = PlayerStats.Query.all()
+     print 'Query done.'
+     #Go through each field, and construct ranking
+     for i in range(1, len(field)):
+        print 'Building stats for ', category[i], '.\n';
+        leaderboardString += (category[i] + '\n')
+        currentRankings = allStats.order_by('-' + field[i])
+        rank = 1
+        for playerStat in currentRankings:
+          if playerStat.name:
+            leaderboardString += (str(rank) + '. ' + playerStat.name + ' ' + str(playerStat.__dict__[field[i]]) + '\n')
+          rank += 1
+          if rank > 5:
+            break
+     print leaderboardString, "\n\n"
+     sleep(30)
